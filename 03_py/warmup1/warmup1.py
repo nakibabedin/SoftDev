fcd05d310440ae21371b07eed26a961d38e8eff8
Nakib Abedin <nabedin30@stuy.edu>
2022-09-22 13:41:31 -0400

    fix bug

--- 03_py/warmup1/warmup1.py
+++ 03_py/warmup1/warmup1.py
@@ -58,7 +58,7 @@ print(front3('abc'))
 
 #diff21
 # Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
-def diff21():
+def diff21(n):
   if(n>21):
     return 2* (n-21)
   else:
