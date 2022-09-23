8bd1bd095e56c8a3b9bac005d4f549d7b404aac2
Nakib Abedin <nabedin30@stuy.edu>
2022-09-23 14:04:30 -0400

    updt k04

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -10,7 +10,6 @@ DISCO:
     *figured out general solution for returning a random devo 
 QCC:
     *will the final dictionary work with our code?
-    *why does ameer suck at coding?
     *how do we ensure that the code will completely randomly chose a devo?
 OPS Summary:
     1. gather all keys from dictionary in a list, keys
@@ -22,14 +21,45 @@ OPS Summary:
 
 import random as rng
 
-krewes = {1:['A', 'B', 'C'], 2:['D', 'E', 'F']}
+#krewes = {1:['A', 'B', 'C'], 2:['D', 'E', 'F']}
+krewes = {
+           2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY", "Ruiwen"], 
+           7:["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "Karen"],
+           8:["ALEKSANDRA",  "NAKIB",  "AMEER",  "HENRY",  "DONALD",  "YAT LONG",  "SEBASTIAN",  "DAVID",  "YUKI",  "SHAFIUL",  "DANIEL",  "SELENA",  "JOSEPH",  "SHINJI",  "RYAN",  "APRIL",  "ERICA",  "JIAN HONG",  "VERIT",  "JOSHUA",  "WILSON",  "AAHAN",  "GORDON",  "JUSTIN",  "MAYA",  "FAIYAZ",  "SHREYA",  "ERIC",  "JEFFERY",  "BRIAN",  "KEVIN",  "SAMSON",  "BRIAN",  "HARRY",  "Wanying", "Kevin"]
+         }
+
+
 def find_devo(krewes):
-    keys = list(krewes.keys())
+    keys = list(krewes)
     index = rng.randint(0, len(keys)-1)
     key = keys[index]
     rando_value = rng.randint(0, len(krewes[key])-1)
-    return krewes[key][rando_value]
+    return "The devo you picked is " + krewes[key][rando_value] + " in period " + str(key)
 
-print(find_devo(krewes))
-    
+def find_devo_pd(krewes):
+    print("Which period do you want a random devo from?")
+    pd = int(input())
+    if(pd != 2 and pd != 7 and pd != 8):
+        print("GG.")
+    rando_value = rng.randint(0, len(krewes[pd])-1)
+    return "The devo you picked is " + krewes[pd][rando_value] + " from your chosen period " + str(pd)
+
+while(True):
+    print("Which of the following methods do you want to run?")
+    print("0: find a random devo from any period")
+    print("1: find a random devo from a period of your choice")    
+program = int(input())
+    if(program == 0):
+        print(find_devo(krewes))
+        input
+    elif(program == 1):
+        print(find_devo_pd(krewes))
+        
+    print("Do you want to continue? Press y for yes, n for no")
+    again = input()
+    if(again != "y"):
+        break
+
+
+
     
