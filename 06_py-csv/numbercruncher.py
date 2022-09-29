9af9822f4b0e258b3af6cbe5120a9a6d71f4a2ff
Nakib Abedin <nabedin30@stuy.edu>
2022-09-29 14:07:31 -0400

    updat 06

---
+++ 06_py-csv/numbercruncher.py
@@ -0,0 +1,14 @@
+# NADH -- Nakib Abedin, Daniel He
+# SoftDev pd08
+# K06 -- working with a txt
+# 2022-29-2022
+#
+# Disco
+# QCC
+# OPS
+# 0) Store all of the information from the CSV file in a dictionary
+# 1) 
+#
+#
+
+
