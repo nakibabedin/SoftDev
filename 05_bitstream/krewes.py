24a6cf17bee3d0d1ae66ff18e419db24bfed884a
Nakib Abedin <nabedin30@stuy.edu>
2022-10-03 14:07:00 -0400

    nakib poopoo

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,32 +1,43 @@
-# 
-# SoftDev
-# K05 --
-# 2022-09-28
+# NADH -- Nakib Abedin, Daniel He
+# SoftDev pd08
+# K05 -- working with a txt file
+# 2022-09-29
 # time spent: 
 
-# DISCO:  
+# DISCO:
+#   0) you can open and store the contents of a txt file in python using the open method
+#   1) the open method returns a file object which can be read using the .read() method
 # QCC:
-# OPS SUMMARY: 
+#   0) if dic[s[0]] is none in line 22, would it just return none or will it add something to none
+# OPS SUMMARY:
+# Populate:
+#   0) Open the file and store its contents in a variable
+#   1) Use the .split method in order to seperate all of the devos across each period
+#   2) Use the .split method again to store the information about each devo in a list 
+#   3) Return the populated dictionary
 import random
 
 def populate():
     file = open("krewes.txt", "r")
     data = file.read().split("@@@") #the list version of file object
-    dic = {}
-    for e in data:
-        s = e.split("$$$")
-        if s[0] in dic:
+    dic = {} #dictionary to be populated
+    for e in data: # loops through a list of lists
+        s = e.split("$$$") # splits information so that it becomes seperate elements in a list
+        #check, only proceed if dict size >0
+        if s[0] in dic: # checks if the key already exists
             dic[s[0]] = dic[s[0]] + [s[1],s[2]]
         else:
             dic[s[0]] = [s[1],s[2]]
-    return dic
+    return dic # return populated dictionary
     
-def randomName():
-    periods = list(krewes)
-    period = random.choice(periods)
-    devo = random.choice(krewes[period])
-    print(devo + " from period " + str(period))
+def randomName(dic):
+    # randomly generates a devo
+    periods = list(dic) # list of all the keys
+    period = random.choice(periods) # randomly selects a period
+    devo = dic[period][0] # the bv first element in the list is the devo
+    ducky = dic[period][1] # the second element is the ducky
+    return(devo + " from period " + str(period) + " whose ducky is " + ducky)
 
 dictionary = populate()
-print(dictionary["7"])
+print(randomName(dictionary))
 
--- 07_teardown/app.py
+++ 07_teardown/app.py
@@ -1,12 +1,21 @@
-# your heading here
+'''
+Charles's Angels::Aleksandra Shifrina, Nakib Abedin, Ameer Alnasser
+SoftDev pd08
+K04 -- RNG + Dictionaries
+2022-09-22
+time spent: 0.5hrs
+'''
+
 
 from flask import Flask
 
 app = Flask(__name__) # Q0: Where have you seen similar syntax in other langs?
 
+
+
 @app.route("/") # Q1: What points of reference do you have for meaning of '/'?
 def hello_world():
-    print(__name__) # Q2: Where will this print to? Q3: What will it print?
+    print(name) # Q2: Where will this print to? Q3: What will it print?
     return "No hablo queso!"  # Q4: Will this appear anywhere? How u know?
 
 app.run()  # Q5: Where have you seen similar constructs in other languages?
@@ -16,12 +25,11 @@ app.run()  # Q5: Where have you seen similar constructs in other languages?
 DISCO:
 
 QCC:
-0. 
-1. 
-2. 
-3. 
-4. 
-5. 
+0.  Line 12 utilized a similar syntax as the java constructor
+1.  the forward slash is utilized when we navigate directories
+2.  the output will be printed inside of the directory with the app, with the #3 name from the constructor
+4.  It will not appear anywhere because we never run the hello_world() function
+5.  java also has object-oriented programming, as seen on line 21 wih the call of a method from app  
 ...
 
 INVESTIGATIVE APPROACH:
