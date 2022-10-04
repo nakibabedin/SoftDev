a3ce98c4b4173f8dd534036588bc0d16e437eb45
Nakib Abedin <nabedin30@stuy.edu>
2022-10-04 14:05:53 -0400

    add k07

--- 07_teardown/app.py
+++ 07_teardown/app.py
@@ -15,7 +15,7 @@ app = Flask(__name__) # Q0: Where have you seen similar syntax in other langs?
 
 @app.route("/") # Q1: What points of reference do you have for meaning of '/'?
 def hello_world():
-    print(name) # Q2: Where will this print to? Q3: What will it print?
+    print(__name__) # Q2: Where will this print to? Q3: What will it print?
     return "No hablo queso!"  # Q4: Will this appear anywhere? How u know?
 
 app.run()  # Q5: Where have you seen similar constructs in other languages?
@@ -24,12 +24,16 @@ app.run()  # Q5: Where have you seen similar constructs in other languages?
 '''
 DISCO:
 
-QCC:
-0.  Line 12 utilized a similar syntax as the java constructor
-1.  the forward slash is utilized when we navigate directories
-2.  the output will be printed inside of the directory with the app, with the #3 name from the constructor
-4.  It will not appear anywhere because we never run the hello_world() function
-5.  java also has object-oriented programming, as seen on line 21 wih the call of a method from app  
+QCC: What is the @ symbol supposed to represent in the code/what is its purpose?
+Does printing __name__ run the app?
+
+
+0. Line 12 utilized a similar syntax as the java constructor.
+1. The forward slash is utilized when we navigate directories.
+2. The output will be printed in the website once the flask app is opened, with the return value printed.
+3. The return value "No hablo queso!" will be printed. 
+4. It will appear on the website once the flask app is opened.
+5. Java has object-oriented programming, as seen on line 21 with the call of a method from app.
 ...
 
 INVESTIGATIVE APPROACH:
