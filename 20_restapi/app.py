16e9c5d377a134eab03f7eb6c6508388e519cd40
nakibabedin <nabedin30@stuy.edu>
2022-11-22 13:43:35 -0500

    add changes

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -5,17 +5,19 @@ K20 -- Restful APIs
 2022-11-21
 time spent: 0.7 hrs
 '''
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def page():
-    x = requests.get("https://api.nasa.gov/planetary/apod?api_key=HXuFz4bCkyUrTLbNRiAEf8C88GNXoCYUvLWHK9LK")
-    print(x)
-    return render_template('main.html', url="https://apod.nasa.gov/apod/image/2211/Butterfly_HubbleOstling_960.jpg")
+key = open('key_nasa.txt')
+url = 'https://api.nasa.gov/planetary/apod'
+json = requests.get(url, params={'api_key':key}).json()
 
-if __name__ == "__main__": #false if this file imported as module
-    #enable debugging, auto-restarting of server when this file is modified
-    app.debug = True
+@app.route('/')
+def page():
+    return render_template("main.html", title = json['title'], explanation = json['explanation'], imgURL = json['url'])
+
+if __name__ == "__main__":
+    app.debug = True 
     app.run()
