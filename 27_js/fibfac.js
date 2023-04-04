fa77eca631b8f1d522c643d226592e775e352c6b
Nakib Abedin <nabedin30@stuy.edu>
2023-04-04 14:07:28 -0400

    add hw27

---
+++ 27_js/fibfac.js
@@ -0,0 +1,28 @@
+// Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon 
+// SoftDev pd0
+// K27 -- Basic functions in JavaScript
+// 2023-04-04t
+// --------------------------------------------------
+
+
+// as a duo...
+// pair programming style,
+// implement a fact and fib fxn
+
+function fact(n) {
+    if (n < 2) {
+        return 1
+    }
+    return fact(n - 1) * n
+}
+
+console.log(fact(2))
+
+//Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T.
+function fib(n){
+    if(n == 0) return 0;
+    if(n <= 2) return 1
+    return fib(n-1) + fib(n-2)
+}
+
+console.log(fib(4))
