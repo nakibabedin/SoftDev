fcb85a6a752d49d9a94eee3e3ace466a949242df
Nakib Abedin <nabedin30@stuy.edu>
2023-04-05 00:21:10 -0400

    add code for fib scm; needs testing

--- 27_js/fibfac.scm
+++ 27_js/fibfac.scm
@@ -6,28 +6,36 @@
 ;Scheme Antecedents for JS work
 
 ;factorial:
-(define fact
-    (lambda(n)
-    (if (< n 2))
-        1
-    (* n fact((- n 1)))
-))
-
+(define
+	fact (lambda(n)
+	(if(< n 2))
+	1
+	(* n (fact(n-1)))
+)
+ 
 (fact 1) ;"...should be  1"
-;(fact 2) ;"...should be  2"
-;(fact 3) ;"...should be  6"
-;(fact 4) ;"...should be  24"
-;(fact 5) ;"...should be  120"
+(fact 2) ;"...should be  2"
+(fact 3) ;"...should be  6"
+(fact 4) ;"...should be  24"
+(fact 5) ;"...should be  120"
 
 
 ;fib:
-;<your team's fib(n) implementation>
+(define 
+	fib (lambda(n)
+	(if (= n 1))
+	1
+	(if (= n 0))
+	0
+	(+ fib(n-1) fib(n-2)))
+)
 
-;(fib 0) ;"...should be  0"
-;(fib 1) ;"...should be  1"
-;(fib 2) ;"...should be  1"
-;(fib 3) ;"...should be  2"
-;(fib 4) ;"...should be  3"
+
+(fib 0) ;"...should be  0"
+(fib 1) ;"...should be  1"
+(fib 2) ;"...should be  1"
+(fib 3) ;"...should be  2"
+(fib 4) ;"...should be  3"
 ;=================================================================
 
  
