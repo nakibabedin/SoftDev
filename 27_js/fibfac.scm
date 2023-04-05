5f63c3c10416184d65d5a193b236117c36eb10f2
Nakib Abedin <nabedin30@stuy.edu>
2023-04-05 00:41:54 -0400

    fix scheme code; finished testing

--- 27_js/fibfac.scm
+++ 27_js/fibfac.scm
@@ -1,3 +1,4 @@
+#lang racket
 ;Team David and Nakib (DAN) | David Chen, Nakib Abedin
 ;SoftDev pd08
 ;K27 -- Basic functions in JavaScript
@@ -9,12 +10,11 @@
 
 ;factorial:
 (define
-	fact (lambda(n)
-	(if(< n 2))
-	1
-	(* n (fact(n-1)))
-)
- 
+        fact (lambda(n)
+        (if(< n 2) 1
+        (* n (fact(- n 1)))
+)))
+
 (fact 1) ;"...should be  1"
 (fact 2) ;"...should be  2"
 (fact 3) ;"...should be  6"
@@ -23,14 +23,11 @@
 
 
 ;fib:
-(define 
-	fib (lambda(n)
-	(if (= n 1))
-	1
-	(if (= n 0))
-	0
-	(+ fib(n-1) fib(n-2)))
-)
+(define
+        fib (lambda(n)
+        (if (or (= n 0) (= n 1)) n
+        (+ (fib(- n 1)) (fib(- n 2))))
+))
 
 
 (fib 0) ;"...should be  0"
@@ -39,5 +36,4 @@
 (fib 3) ;"...should be  2"
 (fib 4) ;"...should be  3"
 ;=================================================================
-
  
